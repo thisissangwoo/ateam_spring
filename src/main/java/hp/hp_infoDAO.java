710d6e4830b39e6075a5c0aa2048e1a228babca9
eun-yeong <zerotey54@gmail.com>
2022-05-24 19:55:42 +0900

    0524 찜하기 기능 DB 조회

--- src/main/java/hp/hp_infoDAO.java
+++ src/main/java/hp/hp_infoDAO.java
@@ -1,21 +1,50 @@
 package hp;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import user.UserChoiceVO;
+
 @Repository
 public class hp_infoDAO {
-	@Autowired
-	@Qualifier("ateam")
-	SqlSession sql;
+	@Autowired @Qualifier("ateam") SqlSession sql;
 	
-	
-	
+	//병원 상세 정보 조회(카카오맵)
 	public Hp_infoVO detailmap(HashMap<String, String> hashmap) {
 		return sql.selectOne("hp.mapper.detailmap", hashmap);
+	}
+	
+	//병원 상세 정보 조회(단순)
+	public Hp_infoVO detailhp(String code) {
+		return sql.selectOne("hp.mapper.detailhp", code);
+	}
+	
+	//병원 상세 정보 찜한 여부 확인
+	public UserChoiceVO checkHeart(HashMap<String, String> check) {
+		return sql.selectOne("hp.mapper.checkheart",check);
+	}
+	//찜한 병원 insert
+	public void insertHeart(HashMap<String, String> check) {
+		 sql.insert("hp.mapper.insertheart",check);
+	}
+	
+	//찜한 병원 update
+	public void updatetHeart(HashMap<String, String> check) {
+			 sql.update("hp.mapper.updateheart",check);
+	}
+	
+	//찜한 병원 delete
+	public void deleteHeart(HashMap<String, String> check) {
+			 sql.delete("hp.mapper.deleteheart",check);
+	}
+	
+	//찜한 병원 전체 조회
+	public List<UserChoiceVO> selectHeart(String user_id) {
+		return sql.selectList("hp.mapper.selectheart",user_id);
 	}
 }
---
+++ src/main/java/user/UserChoiceVO.java
@@ -0,0 +1,47 @@
+package user;
+
+public class UserChoiceVO {
+	String user_id,hp_code,ch_date, hp_name,hp_addr ;
+
+	public String getHp_name() {
+		return hp_name;
+	}
+
+	public void setHp_name(String hp_name) {
+		this.hp_name = hp_name;
+	}
+
+	public String getHp_addr() {
+		return hp_addr;
+	}
+
+	public void setHp_addr(String hp_addr) {
+		this.hp_addr = hp_addr;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getHp_code() {
+		return hp_code;
+	}
+
+	public void setHp_code(String hp_code) {
+		this.hp_code = hp_code;
+	}
+
+	public String getCh_date() {
+		return ch_date;
+	}
+
+	public void setCh_date(String ch_date) {
+		this.ch_date = ch_date;
+	}
+	
+	
+}
