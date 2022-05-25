49d2251d7178797dade4ed3d1051438008221740
AlphaGo <98372774+AlphaGoJava@users.noreply.github.com>
2022-05-25 19:40:31 +0900

    05 25 19:40 박천일 최종본
    05 25 19:40 박천일 최종본

--- src/main/java/com/hanul/anafor/SearchController.java
+++ src/main/java/com/hanul/anafor/SearchController.java
@@ -1,5 +1,6 @@
 package com.hanul.anafor;
 
+
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,24 +17,45 @@ import com.google.gson.Gson;
 
 import hp.hpVO;
 
+
+
 @Controller
 public class SearchController {
+   
 
-	Gson gson = new Gson();
+   Gson gson = new Gson();
+   
+   @Autowired @Qualifier("ateam") private SqlSession sql2;
 
-	@Autowired
-	@Qualifier("ateam")
-	private SqlSession sql2;
-
-	//
-	@ResponseBody
-	@RequestMapping(value = "/hash", produces = "application/json;charset=UTF-8")
-	public String search(HttpServletRequest req) {
-		String keyword = req.getParameter("query");
-		System.out.println(req.getParameter("query"));
-		List<hpVO> list = sql2.selectList("search.mapper.search", keyword);
-		System.out.println(gson.toJson(list));
-		return gson.toJson(list);
-	}
-
+   
+   @ResponseBody
+   @RequestMapping(value="/hash", produces = "application/json;charset=UTF-8")
+   public String search(HttpServletRequest req) {
+	   hpVO searchVO = new hpVO();
+	   searchVO.setHp_name( req.getParameter("query") );
+	   searchVO.setHp_x( req.getParameter("setHp_x") );
+	   searchVO.setHp_y( req.getParameter("setHp_y") );
+	   //안드에서 검색어 , 지금 위도 , 경도를 hpVO에 담아서 => String(Json)으로 보내줌 
+	   //Spring에서는 json=>hpVO로 바꿔서 검색조건으로 사용
+	   
+	   //String keyword = req.getParameter("query");
+	   //System.out.println(req.getParameter("query"));
+	   List<hpVO> list = sql2.selectList("search.mapper.search",searchVO);
+	   System.out.println(gson.toJson(list));
+	   return gson.toJson(list); 
+   }
+   
+//   @ResponseBody
+//   @RequestMapping(value="/review", produces = "application/json;charset=UTF-8")
+//   public String review(HttpServletRequest req) {
+//	   
+//	   hpVO searchVO = new hpVO();
+//	   searchVO.setHp_name( req.getParameter("query") );
+//	   
+//	   List<hpVO> list = sql2.selectList("search.mapper.review",searchVO);
+//	   System.out.println(gson.toJson(list));
+//	   
+//	   return gson.toJson(list); 
+//   }
+  
 }
