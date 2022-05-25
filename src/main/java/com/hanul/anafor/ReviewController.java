9e53a9a74d38c96bf654ab4776968c62ac867c6b
eun-yeong <80459691+euzny@users.noreply.github.com>
2022-05-26 01:13:59 +0900

    리뷰 조회 코드 추가
    조회 코드 수정 필요

--- src/main/java/com/hanul/anafor/ReviewController.java
+++ src/main/java/com/hanul/anafor/ReviewController.java
@@ -1,5 +1,7 @@
 package com.hanul.anafor;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +21,24 @@ public class ReviewController {
 	
 	@Autowired ReviewDAO dao;
 	
+	//리뷰 등록
 	@ResponseBody
 	@RequestMapping(value="/insert.review", produces = "application/json;charset=UTF-8")
 	public String insertReview(HttpServletRequest req) {
 		ReviewVO vo = gson.fromJson(req.getParameter("vo"), ReviewVO.class );
 		dao.insertReview(vo);
 		return "";
+	}
+	//전체 리뷰 조회
+	@ResponseBody
+	@RequestMapping(value="/selectAll.review",produces="application/json;chartset=UTF-8")
+	public String selectAllReview(HttpServletRequest req) {
+		return gson.toJson(dao.selectAllReview(req.getParameter("code")));
+	}
+	//해당 회원 리뷰 조회
+	@ResponseBody
+	@RequestMapping(value="/selectMy.review",produces="application/json;chartset=UTF-8")
+	public String selectMyReview(HttpServletRequest req) {
+		return gson.toJson(dao.selectMyReview(req.getParameter("user_id")));
 	}
 }
--- src/main/java/review/ReviewDAO.java
+++ src/main/java/review/ReviewDAO.java
@@ -1,5 +1,7 @@
 package review;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,7 +13,17 @@ public class ReviewDAO {
 	
 	@Autowired @Qualifier("ateam") SqlSession sql;
 	
+	//리뷰 등록
 	public void insertReview(ReviewVO vo) {
 		sql.insert("review.mapper.insert",vo);
+	}
+	
+	//리뷰 조회(전체 조회)
+	public List<ReviewVO> selectAllReview(String code) {
+		return sql.selectList("review.mapper.selectAll",code);
+	}
+	//리뷰 조회(해당 회원 조회)
+	public List<ReviewVO> selectMyReview(String code) {
+		return sql.selectList("review.mapper.selectMy",code);
 	}
 }
