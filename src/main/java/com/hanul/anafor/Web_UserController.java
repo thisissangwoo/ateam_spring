8497d6837f015b56ef64fb7b5217c8e036bfe19d
eun-yeong <zerotey54@gmail.com>
2022-06-16 11:57:07 +0900

    소셜 로그인 기능 수정

--- src/main/java/com/hanul/anafor/Web_UserController.java
+++ src/main/java/com/hanul/anafor/Web_UserController.java
@@ -174,6 +174,7 @@ public class Web_UserController {
 				json = json.getJSONObject("kakao_account");
 				System.out.println(json);
 				vo.setUser_id(json.getString("email"));
+				vo.setUser_pw(json.getString("email"));
 				vo.setSocial_type("user_kakao");
 				vo.setUser_name(json.getJSONObject("profile").getString("nickname"));
 				vo.setUser_gender(json.has("gender") && json.getString("gender").equals("female")? "여": "남");
@@ -284,6 +285,7 @@ public class Web_UserController {
 				UserVO vo = new UserVO();
 				vo.setSocial_type("user_naver");
 				vo.setUser_id(json.getString("email"));
+				vo.setUser_pw(json.getString("email"));
 				vo.setUser_name(json.getString("name"));
 				vo.setSocial_id(json.getString("id"));
 				String phone = json.getString("mobile"); //전화번호 하이픈 제거
